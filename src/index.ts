1263afcb43ffd44086e51a701e6546e9c96d7895
Orelso Vitam <orelso.vitam@avanio.com>
2023-02-27 12:43:51 +0200

    basic finished

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,77 @@
-import { v4 as uuidV4 } from 'uuid'
+import { v4 as uuidV4 } from 'uuid';
+import confetti from 'canvas-confetti';
 
 type Task = {
-  id: string
-  title: string
-  completed: boolean
-  createdAt: Date
-} 
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
-const list = document.querySelector<HTMLUListElement>('#list')
-const form = document.getElementById('new-task-form') as HTMLFormElement | null
-const input = document.querySelector<HTMLInputElement>('#new-task-title')
-const tasks: Task[] = loadTasks()
-tasks.forEach(addListItem)
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.getElementById('new-task-form') as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
 
-form?.addEventListener('submit', e => {
-  e.preventDefault()
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-  if( input?.value == "" || input?.value == null) 
-  return
+  if (input?.value == '' || input?.value == null) return;
 
   const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
-    createdAt: new Date()
-  }
+    createdAt: new Date(),
+  };
 
-  tasks.push(newTask)
-  saveTasks()
-  
-  addListItem(newTask)
-  input.value = ""
-})
+  tasks.push(newTask);
+  saveTasks();
+
+  addListItem(newTask);
+  input.value = '';
+});
 
 function addListItem(task: Task) {
-  const item = document.createElement('li')
-  const label = document.createElement('label')
-  const checkbox = document.createElement('input')
-  checkbox.addEventListener("change", ( ) => {
-    task.completed = checkbox.checked
-    saveTasks()
-  })
-  checkbox.type = "checkbox"
-  checkbox.checked = task.completed
-  label.append(checkbox, task.title)
-  item.append(label)
-  list?.append(item)
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = '🗑️';
+  deleteButton.addEventListener('click', () => {
+    const index = tasks.findIndex((t) => t.id === task.id);
+    if (index !== -1) {
+      tasks.splice(index, 1);
+      saveTasks();
+      item.remove();
+    }
+  });
+  item.append(deleteButton);
+
+  list?.append(item);
 }
 
 function saveTasks() {
-  localStorage.setItem("TASKS", JSON.stringify(tasks))
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
 }
 
 function loadTasks(): Task[] {
-  const taskJSON = localStorage.getItem("TASKS")
-  if (taskJSON == null) return []
-  return JSON.parse(taskJSON)
+  const taskJSON = localStorage.getItem('TASKS');
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import confetti from 'canvas-confetti';
-
-// confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-//   resize: true,
-//   useWorker: true,
-// })({ particleCount: 20000, spread: 2000 });
+confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
+  resize: true,
+  useWorker: true,
+})({ particleCount: 2000, spread: 2000 });
